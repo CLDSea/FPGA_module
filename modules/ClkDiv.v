d09651f66ea6b03848b4c8900a777871d30c41ad
Fu Yuhao <99805619+CLDSea@users.noreply.github.com>
2023-05-11 15:52:18 +0800

    Add files via upload

--- modules/ClkDiv.v
+++ modules/ClkDiv.v
@@ -10,6 +10,7 @@ module ClkDiv
 		 );
 
 parameter [31: 0]cnt_max = 32'd1000;
+parameter [31: 0]cnt_threshold = 32'd500;
 
 //wire
 
@@ -18,6 +19,7 @@ parameter [31: 0]cnt_max = 32'd1000;
 //计数分频
 //fre_out=fre_in/cnt_max
 //cnt=0,1,2,...,cnt_max-1
+//一般(cnt_max-1)>>1=cnt_threshold-1
 always@(posedge clk or posedge phase_rst or negedge rst_n)
 begin
 	if (!rst_n)
@@ -39,7 +41,7 @@ begin
 			begin
 				clk_div <= 1'd1;
 			end
-			else if (cnt == ((cnt_max - 1'd1) >> 1'd1))
+			else if (cnt == cnt_threshold - 1'd1)
 			begin
 				clk_div <= 1'd0;
 			end
