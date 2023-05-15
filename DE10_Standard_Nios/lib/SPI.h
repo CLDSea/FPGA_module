636fb5d3731d0508b0f2e0bec127f6fa1822ae9e
CLDSea <99805619+CLDSea@users.noreply.github.com>
2023-05-15 17:22:24 +0800

    Add files via upload

---
+++ DE10_Standard_Nios/lib/SPI.h
@@ -0,0 +1,13 @@
+/*
+ * SPI.h
+ *
+ *  Created on: 2023Äê5ÔÂ14ÈÕ
+ *      Author: FuYuhao
+ */
+
+#ifndef SPI_H_
+#define SPI_H_
+
+void SpiTX(alt_u64 data,int len);
+
+#endif /* LIB_SPI_H_ */
---
+++ DE10_Standard_Nios/lib/UART.h
@@ -0,0 +1,18 @@
+/*
+ * UART.h
+ *
+ *  Created on: 2023Äê5ÔÂ8ÈÕ
+ *      Author: FuYuhao
+ */
+
+#ifndef UART_H_
+#define UART_H_
+
+extern int ctrl_value;
+
+void UartIrqInit(long baud);
+void UartIrq();
+
+void UartTX(char str[]);
+
+#endif /* UART_H_ */
