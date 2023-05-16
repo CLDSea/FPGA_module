f6931b37a9cd8f2a07c78a845972ba9c28f9b985
CLDSea <99805619+CLDSea@users.noreply.github.com>
2023-05-16 15:53:14 +0800

    Add files via upload

--- DE10_Standard_Nios/lib/AD9910.c
+++ DE10_Standard_Nios/lib/AD9910.c
@@ -10,9 +10,11 @@
 #include "SPI.h"
 #include <math.h>
 
-//PCB中CS_N与IO_RESET相连，不能使用spi ip核中的CS_N管脚，使用IO_UPDATE管脚代替CS_N管脚
+//PCB中CS_N与IO_RESET相连，不能使用spi ip核中的CS_N管脚，使用IO_UPDATE管脚代替CS_N管脚，需要调节延时
 void AD9910Init()
 {
+	while(!(IORD(SPI_BASE,2)&0x40));
+	usleep(1);
 	IOWR(IO_UPDATE_OUT_BASE, 0, 0);
 	
 	//CFR1
@@ -20,27 +22,33 @@ void AD9910Init()
 	SpiTX(0x00400000, 4);
 	//CFR3
 	SpiTX(0x02, 1);
-	SpiTX(0x05084132, 4);
+	SpiTX(0x050F4132, 4);
 	//I/O_Update
 	SpiTX(0x04, 1);
-	SpiTX(0x00000004, 4);
+	SpiTX(0x000061A8, 4);
 	//CFR2
 	SpiTX(0x01, 1);
 	SpiTX(0x01C00000, 4);
 
+	while(!(IORD(SPI_BASE,2)&0x40));
+	usleep(1);
 	IOWR(IO_UPDATE_OUT_BASE, 0, 1);
 }
 
 void AD9910FrePhaseAmp(int fre, int phase, double amp)
 {
-	alt_u64 FTW = round(fre * 4.294967296); //0-500_000_000
-	alt_u64 POW = round(phase * 182.044444444); //0-360
-	alt_u64 ASF = round(amp * 16383); //0-1
+	alt_u64 FTW = ((alt_u64)round(fre * 4.294967296))&0xFFFFFFFF; //0-500_000_000
+	alt_u64 POW = ((alt_u64)round(phase * 182.044444444))&0xFFFF; //0-360
+	alt_u64 ASF = ((alt_u64)round(amp * 16383.0))&0x3FFF; //0-1
 
+	while(!(IORD(SPI_BASE,2)&0x40));
+	usleep(1);
 	IOWR(IO_UPDATE_OUT_BASE, 0, 0);
 
 	SpiTX(0x15, 1);
 	SpiTX((ASF << 48) | (POW << 32) | FTW, 8);
 
+	while(!(IORD(SPI_BASE,2)&0x40));
+	usleep(1);
 	IOWR(IO_UPDATE_OUT_BASE, 0, 1);
 }
